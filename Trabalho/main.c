329cc7560601d728e278b092f5c584104e4d4f7b
Lucas_Silva <lucas2751ferreira@gmail.com>
2024-09-20 16:53:23 +0100

    comentarios nas funcoes

--- Trabalho/main.c
+++ Trabalho/main.c
@@ -2,38 +2,25 @@
 #include <stdlib.h>
 #include <locale.h>
 #define PI 3.14
-
-#include <stdio.h>
-#include <stdlib.h>
-
-
-#include <stdio.h>
-#include <math.h>
-
-
+// Divaldo calculo Triangulo
 float calcular_area(float base, float altura) {
     return (base * altura) / 2;
 }
 
-
 float calcular_perimetro(float lado1, float lado2, float lado3) {
     return lado1 + lado2 + lado3;
 }
-
-
+// Flavio Calculo Pentagono
 float perimetro()
 {
-
  float area,lado,perimetro;
 
   printf("Digite o lado do pentagono:");
   scanf("%f",&lado);
   perimetro = lado * 5;
 
-
    printf("O perimetro do pentagono e %1.f cm",perimetro);
   return perimetro;
-
 
 
 }
@@ -49,12 +36,10 @@ float area()
 
   area =(p*lado)/2;
 
-
    printf("A area do pentagono e %1.f cm",area);
    return area;
 }
-
-
+// Lucas Silva Circulo
 float circulo(){
     float r;
     printf("Insira o valor do raio\n");
@@ -68,10 +53,6 @@ int main()
     setlocale(LC_ALL, "Portuguese");
 
     printf("A área do círculo é: %.2f\n",circulo());
-
-
-
-// vamos ver se esta funcionando
 
 
     return 0;
