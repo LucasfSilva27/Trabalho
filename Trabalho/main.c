e6f7b1f790ca71e25a59870d85a3b71548bc3be3
Lucas_Silva <lucas2751ferreira@gmail.com>
2024-09-20 14:57:34 +0100

    Funcao Circulo

--- Trabalho/main.c
+++ Trabalho/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#define PI 3,14
+
+float circulo(){
+    float b,a;
+    printf ("Digite o Raio: ");
+    scanf("%f",&a);
+    return b = PI*(a*2);
+}
 
 int main()
 {
+    float a;
     setlocale(LC_ALL, "Portuguese");
-
-
+    //por em um case para a funcao circulo
+    a = circulo(a);
+    printf("A %0.f",a);
 
     return 0;
 }
