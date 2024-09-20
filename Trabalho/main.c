88e957bd0c51db3572097b672aa696fb6f56b069
fpereira <flavioteixeira2000@gmail.com>
2024-09-20 16:19:10 +0100

    parte flavio

--- Trabalho/main.c
+++ Trabalho/main.c
@@ -3,6 +3,43 @@
 #include <locale.h>
 #define PI 3.14
 
+#include <stdio.h>
+#include <stdlib.h>
+
+float perimetro()
+{
+
+ float area,lado,perimetro;
+
+  printf("Digite o lado do pentagono:");
+  scanf("%f",&lado);
+  perimetro = lado * 5;
+
+
+   printf("O perimetro do pentagono e %1.f cm",perimetro);
+  return perimetro;
+
+
+
+}
+
+float area()
+{
+
+  float area,p,lado;
+  printf("Digite a apotema do pentagono:");
+  scanf("%f",&p);
+  printf("Digite o lado do pentagono:");
+  scanf("%f",&lado);
+
+  area =(p*lado)/2;
+
+
+   printf("A area do pentagono e %1.f cm",area);
+   return area;
+}
+
+
 float circulo(){
     float r;
     printf("Insira o valor do raio\n");
